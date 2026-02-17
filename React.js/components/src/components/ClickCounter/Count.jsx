fd3eda846f7bb0c30f2ca337385df0c785b0557e
Ritesh007-max <riteshworking247@gmail.com>
2026-02-17 14:22:23 +0530

    Created ClickCounter Using useEffect & useReducer

--- React.js/components/src/components/ClickCounter/Count.jsx
+++ React.js/components/src/components/ClickCounter/Count.jsx
@@ -1,27 +1,83 @@
-import React from 'react'
-import { useState } from 'react'
-import { useEffect } from 'react';
-import "./Count.css"
+import React, { useState, useEffect, useReducer } from "react";
+import "./Count.css";
 
+function reducer(state, action) {
 
-function Count() {
-  
-  var[count, setCount] = useState(0);
-  function handleCount() {
-    setCount(++count);
+  switch (action.type) {
+    case "handleInc":
+      console.log("HandleInc function called ..... ");
+      return { ...state, count: state.count + 1 };
+    case "handleDec":
+      console.log("HandleDec function called .... ");
+      return { ...state, count: state.count - 1 };
   }
 
-  useEffect(() => {
-    console.log("useEffect Runs");
-  }, [count]);
-
-    return (
-
-    <div className='counter-div'>
-      <p className='count-para'>Count: {count}</p>
-      <button className='count-btn' onClick={handleCount}>Click</button>
-    </div>
-  )
+  localStorage.setItem("count", state.count);
 }
 
-export default Count
+const initaialState = {
+  count: 0,
+}
+
+function Count() {
+  const [state, dispatch] = useReducer(reducer, initaialState);
+  
+  return (
+    <div>
+      <p>Count: {state.count}</p>
+      <button
+        onClick={() => {
+          dispatch({ type: "handleInc" });
+        }}
+      >
+        Inc
+      </button>
+      <button
+        onClick={() => {
+          dispatch({ type: "handleDec" });
+        }}
+      >
+        Dec
+      </button>
+    </div>
+  );
+}
+
+// function Count() {
+
+//   var[count, setCount] = useState(0);
+
+//   function handleCount(id) {
+
+//     if (id == "Inc") {
+//       setCount(++count);
+//     }
+//     else if (id == "Dec") {
+//       setCount(--count);
+//     }
+//     else if (id == "Reset") {
+//       setCount(0);
+//     }
+//   }
+
+//   useEffect(() => {
+//     const data = JSON. parse(localStorage.getItem("Count"));
+//     setCount(data);
+//   }, []);
+
+//   useEffect(() => {
+//     localStorage.setItem('count:', count);
+//   }, [count]);
+
+//     return (
+
+//     <div className='counter-div'>
+//       <p className='count-para'>Count: {count}</p>
+//       <button className='count-btn' onClick={() => handleCount("Inc")}>increment</button>
+//       <button className='count-btn' onClick={() => handleCount("Dec")}>decrement</button>
+//       <button className='count-btn' onClick={() => handleCount("Reset")}>reset</button>
+//     </div>
+//   )
+// }
+
+export default Count;
